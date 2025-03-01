37d8bab44ad4104a62028670508bcab867930099
IsaacC808 <isaac808hba@gmail.com>
2025-03-01 19:53:37 +0000

    class

---
+++ script.js
@@ -0,0 +1,22 @@
+let header = document.getElementById("main-title");
+
+header.style.textAlign = "center";
+
+let description = document.getElementById("description");
+
+description.style.textAlign ="center";
+
+let pizzalist = document.getElementById("pizza-list");
+
+pizzalist.style.justifyContent = "space-around";
+
+let pizzaImages = document.getElementsByClassName("pizza-img");
+
+pizzaImages[1].src = "images/veggie.png";
+
+let pizzas = document.getElementsByClassName("pizza");
+
+pizzas[0].style.color = "red";
+pizzas[2].style.color ="orange";
+
+
